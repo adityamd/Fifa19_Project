9e25b1f943bf76b783e3463246d1956399b47a71
adityamd <aditya.t2210@gmail.com>
2020-03-03 19:45:13 +0530

    Latest Changes

--- Project/Fifa_Site/Fifa_Site/settings.py
+++ Project/Fifa_Site/Fifa_Site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#pdewl&dk&ru(dcdot_#28jl-n56!66*)94f1ca2^k&^$1n(wv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = ['13.232.171.133','fifa19.gq','127.0.0.1']
 
