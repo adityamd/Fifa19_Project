8dac97bb3d8aef888167e5c4aca78ab273cd9287
adityamd <aditya.t2210@gmail.com>
2020-03-03 07:54:06 +0530

    Responsiveness Included

--- Project/Fifa_Site/Fifa_Site/settings.py
+++ Project/Fifa_Site/Fifa_Site/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Fifa_Site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/var/www/html/Project/Fifa_Site/templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
